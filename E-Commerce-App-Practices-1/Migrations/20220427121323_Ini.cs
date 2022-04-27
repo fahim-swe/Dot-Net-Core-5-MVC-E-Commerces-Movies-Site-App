08c4fe077cd8510ebadf1dbd28db3bb5e5fcc028
fahim <fahim@FAHIM>
2022-04-27 20:02:04 +0600

    Recreate database migration file using command

---
+++ E-Commerce-App-Practices-1/Migrations/20220427121323_Ini.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace E_Commerce_App_Practices_1.Migrations
+{
+    public partial class Ini : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Movies",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Movies",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
