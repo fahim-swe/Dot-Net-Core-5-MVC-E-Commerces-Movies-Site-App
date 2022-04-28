cb4b5fb7cf0a3621e034f3a62ccb0ec672e8325e
fahim <fahim@FAHIM>
2022-04-28 23:47:32 +0600

    adding IActorsService Interface

---
+++ E-Commerce-App-Practices-1/Data/Services/IActorsService.cs
@@ -0,0 +1,16 @@
+﻿using E_Commerce_App_Practices_1.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace E_Commerce_App_Practices_1.Data.Services
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> getAll();
+
+        Actor getById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
