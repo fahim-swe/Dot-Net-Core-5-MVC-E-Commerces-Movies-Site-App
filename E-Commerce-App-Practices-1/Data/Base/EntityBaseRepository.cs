c584a27bf84ec1d7edf7cc1f42078432b4d6709c
fahim <fahim@FAHIM>
2022-05-10 20:02:51 +0600

    Add Get All Async & GetByIdAsync method

--- E-Commerce-App-Practices-1/Data/Base/EntityBaseRepository.cs
+++ E-Commerce-App-Practices-1/Data/Base/EntityBaseRepository.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace E_Commerce_App_Practices_1.Data.Base
 {
     public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
+        private readonly AppDbContext _context;
+
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public Task AddAsync(T entity)
         {
             throw new System.NotImplementedException();
@@ -15,16 +23,22 @@ namespace E_Commerce_App_Practices_1.Data.Base
             throw new System.NotImplementedException();
         }
 
-        public Task<IEnumerable<T>> getAllAsync()
+        public async Task<IEnumerable<T>> getAllAsync()
         {
-            throw new System.NotImplementedException();
+            var result = await _context.Set<T>().ToListAsync();
+
+            return result;
         }
 
-        public Task<T> getByIdAsync(int id)
+        public async Task<T> getByIdAsync(int id)
         {
-            throw new System.NotImplementedException();
+            var resutl = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            return resutl;
         }
 
+
+      
+       
         public Task<T> UpdateAsync(int id, T entity)
         {
             throw new System.NotImplementedException();
