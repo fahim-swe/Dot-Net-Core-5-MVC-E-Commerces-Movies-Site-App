c25b9d4df4a4ac1c515bac23a55d61169951b755
fahim <fahim@FAHIM>
2022-05-11 10:58:56 +0600

    Producers Service (class)

---
+++ E-Commerce-App-Practices-1/Data/Services/ProducersService.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce_App_Practices_1.Data;
+using E_Commerce_App_Practices_1.Data.Base;
+using E_Commerce_App_Practices_1.Data.Services;
+using E_Commerce_App_Practices_1.Models;
+
+namespace E_Commerce_App_Practices_1.Data.Services
+{
+    public class ProducersService: EntityBaseRepository<Producer>, IProducersService
+    {
+        public ProducersService (AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
+
+
