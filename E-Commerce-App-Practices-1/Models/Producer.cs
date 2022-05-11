49eaf387e3465cb0769c0e251c39c8647a3ccdc0
fahim <fahim@FAHIM>
2022-05-11 12:00:04 +0600

    add validation

--- E-Commerce-App-Practices-1/Models/Producer.cs
+++ E-Commerce-App-Practices-1/Models/Producer.cs
@@ -8,8 +8,19 @@ namespace E_Commerce_App_Practices_1.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Display(Name = "Profile Picture URI")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
+
+
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name mush be between 3 and 50 chars")]
         public string FullName { get; set; }
+
+        [Display(Name = "Bio")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
         // Relationships
