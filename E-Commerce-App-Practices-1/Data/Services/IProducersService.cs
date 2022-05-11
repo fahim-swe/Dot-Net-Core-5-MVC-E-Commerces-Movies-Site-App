3580f6c1d48146e325a10a63401d18ad54e446e4
fahim <fahim@FAHIM>
2022-05-11 10:58:43 +0600

    IProducersService (interfaces)

---
+++ E-Commerce-App-Practices-1/Data/Services/IProducersService.cs
@@ -0,0 +1,8 @@
+﻿using E_Commerce_App_Practices_1.Models;
+
+namespace E_Commerce_App_Practices_1.Data.Base
+{
+    public interface IProducersService : IEntityBaseRepository<Producer>
+    {
+    }
+}
