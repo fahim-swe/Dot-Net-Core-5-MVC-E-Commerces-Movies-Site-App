946383ffd0def98328568a668b7e5df0bab38595
fahim <fahim@FAHIM>
2022-05-12 20:19:05 +0600

    add movies service class

---
+++ E-Commerce-App-Practices-1/Data/Services/MoviesService.cs
@@ -0,0 +1,27 @@
+﻿using E_Commerce_App_Practices_1.Data.Base;
+using E_Commerce_App_Practices_1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace E_Commerce_App_Practices_1.Data.Services
+{
+    public class MoviesService: EntityBaseRepository<Movie>, IMoviesService
+    {
+        private readonly AppDbContext _context;
+        public MoviesService(AppDbContext context): base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Movie> GetMovieByIdAsync(int id)
+        {
+            var movieDetails = _context.Movies.Include(c => c.Cinema)
+                .Include(p => p.Producer)
+                .Include(am => am.Actors_Movies)
+                .ThenInclude(a => a.Actor)
+                .FirstOrDefaultAsync(n => n.Id == id);
+
+            return await movieDetails;
+        }
+    }
+}
