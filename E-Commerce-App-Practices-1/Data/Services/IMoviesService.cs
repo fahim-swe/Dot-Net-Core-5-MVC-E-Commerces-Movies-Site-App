2b345aef363ea0ef5ebe68e3ae3979bf8296136c
fahim <fahim@FAHIM>
2022-05-12 20:18:49 +0600

    add movies service Interface

---
+++ E-Commerce-App-Practices-1/Data/Services/IMoviesService.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce_App_Practices_1.Data.Base;
+using E_Commerce_App_Practices_1.Models;
+using System.Threading.Tasks;
+
+namespace E_Commerce_App_Practices_1.Data.Services
+{
+    public interface IMoviesService : IEntityBaseRepository<Movie>
+    {
+        Task<Movie> GetMovieByIdAsync(int id);
+    }
+}
