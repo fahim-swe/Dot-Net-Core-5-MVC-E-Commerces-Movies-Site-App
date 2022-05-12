8f1ec0a9d877af14969d9eb6e2f28b5b2e4f8c11
fahim <fahim@FAHIM>
2022-05-12 22:31:26 +0600

    add GetNewMovieDropdownValues method in IMoviesService Interface

--- E-Commerce-App-Practices-1/Data/Services/IMoviesService.cs
+++ E-Commerce-App-Practices-1/Data/Services/IMoviesService.cs
@@ -1,4 +1,5 @@
 ﻿using E_Commerce_App_Practices_1.Data.Base;
+using E_Commerce_App_Practices_1.Data.ViewModels;
 using E_Commerce_App_Practices_1.Models;
 using System.Threading.Tasks;
 
@@ -7,5 +8,6 @@ namespace E_Commerce_App_Practices_1.Data.Services
     public interface IMoviesService : IEntityBaseRepository<Movie>
     {
         Task<Movie> GetMovieByIdAsync(int id);
+        Task<NewMovieDropdownsVM> GetNewMovieDropdownValues();
     }
 }
