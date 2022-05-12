6c1d2629392b99dd0f9df166542613887c372d25
fahim <fahim@FAHIM>
2022-05-12 22:29:21 +0600

    create new movieDropdowns VM class

---
+++ E-Commerce-App-Practices-1/Data/ViewModels/NewMovieDropdownsVM.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce_App_Practices_1.Models;
+using System.Collections.Generic;
+
+namespace E_Commerce_App_Practices_1.Data.ViewModels
+{
+    public class NewMovieDropdownsVM
+    {
+
+        public NewMovieDropdownsVM()
+        {
+            Producers = new List<Producer>();
+            Cinemas = new List<Cinema>();
+            Actors = new List<Actor>();
+        }
+
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+
+        public List<Actor> Actors { get; set; }
+    }
+}
