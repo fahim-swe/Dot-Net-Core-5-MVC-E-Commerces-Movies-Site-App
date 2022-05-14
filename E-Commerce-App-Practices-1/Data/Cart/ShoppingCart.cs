31601eb238d301e6b59b532d2432736fa3635467
fahim <fahim@FAHIM>
2022-05-14 20:06:51 +0600

    Setting up ShoppingCart Class-. GetShoppingCartItems and GetShoppingCartTotal Amount sum

---
+++ E-Commerce-App-Practices-1/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using E_Commerce_App_Practices_1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E_Commerce_App_Practices_1.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            var result = ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(x => x.ShoppingCartId == ShoppingCartId).Include(x => x.Movie).ToList());
+            return result;
+        }
+
+        public double GetShoppingCartTotal()
+        {
+            var result = _context.ShoppingCartItems.Where(x => x.ShoppingCartId == ShoppingCartId).Select(x => x.Movie.Price).Sum();
+            return result;
+        }
+    }
+}
